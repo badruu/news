fd4cbe961db6abbe06dfad657881d20dfdc005dc
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-15 15:28:14 +0300

    change api position

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
-app = Flask(__name__,instance_relative_config = True)
 
 
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
 
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap()
 
+def create_app(config_name):
+    app = Flask(__name__)
 
-from app import views
-from app import error
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # from .request import configure_request
+    # configure_request(app)
+
+    return app
