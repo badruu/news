a1f76ef221f942a8bb84cd5b0d9cdaa398a873cc
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-15 15:31:40 +0300

    group source class and article to one file

---
+++ app/models.py
@@ -0,0 +1,28 @@
+class Source:
+    '''
+    class source to define source objects
+    '''
+
+    def __init__(self,id,name,description,url,category,language,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.langauge = language
+        self.country = country
+
+class Article:
+    '''
+    class to define article objects
+    '''
+    def __init__(self,id,name,author,title,description,url,urlToImage,publishedAt,content):
+        self.id=id
+        self.name=name
+        self.author=author
+        self.title=title
+        self.description=description
+        self.url=url
+        self.urlToImage=urlToImage
+        self.publishedAt=publishedAt
+        self.content=content
