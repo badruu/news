15398936f5425c92e61404794f8eb4214477c929
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-15 15:41:58 +0300

    change file structure

--- app/error.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from app import app
-
-@app.errorhandler(404)
-def error(error):
-    '''
-    Function to render the 404 page
-    '''
-    return render_template('error.html'),404
