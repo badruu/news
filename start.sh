daf97048ce87c1926fedf9be50b6120a5a168232
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-16 10:15:20 +0300

    remove secret key

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export NEWS_API_KEY='d2b77818e9674bfd91dc2114b7540de0'
-export SECRET_KEY='badar110'
+# export SECRET_KEY='badar110'
 
 gunicorn --timeout 120 --workers 3
 python3.7 manage.py server
