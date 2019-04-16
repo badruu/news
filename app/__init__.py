cbb9dfab191902b9cbba552bb28578c0ecc8f068
badrubadri92@gmail.com <baddrubadri92@gmail.com>
2019-04-16 10:41:13 +0300

    add s to template

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_bootstrap import Bootstrap
 bootstrap = Bootstrap()
 
 def create_app(config_name):
-    app = Flask(__name__, template_folder='template')
+    app = Flask(__name__, template_folder='templates')
 
     app.config.from_object(config_options[config_name])
 
